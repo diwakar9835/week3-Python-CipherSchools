790b52e25ecef1dfed058589874bfe6eadc9bf17
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-12-22 21:15:36 +0530

    Intro to Sets in Python

---
+++ class129-Intro-to-Sets-inPython-CipherSchools.py
@@ -0,0 +1,23 @@
+# set data type
+# unordered collection of unique items
+
+s = {1,2,3,2}
+print(s[1])
+l=[1,2,3,4,5,5,5,5,6,7,7,8]
+# remove duplicate
+s2=list(set(l))
+print(s2)
+
+s.add(4)
+s.add(5)
+print(s)
+
+# s.remove(3)
+# s.discard(5)
+# print(s)
+
+# s.clear()
+# print(s)
+
+# s1=s.copy()
+# print(s)
